fad2c020e8eaf66216e297bc1df4bcbf3dafb1cb
Viren Gupta <viren.gupta@newellco.com>
2020-09-17 12:16:50 +0000

    Initial camera funx added

--- webapp/controller/MaterialDetails.controller.js
+++ webapp/controller/MaterialDetails.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/Fragment",
-	"sap/m/MessageToast"
-], function (Controller, Fragment, MessageToast) {
+	"sap/m/MessageToast",
+	"com/newell/fiori/HelloWorld/libs/Download",
+	"sap/m/Dialog",
+], function (Controller, Fragment, MessageToast, Download, Dialog) {
 	"use strict";
 
 	return Controller.extend("com.newell.fiori.HelloWorld.controller.MaterialDetails", {
@@ -183,6 +185,82 @@ sap.ui.define([
 		
 		goBack: function () {
 			this.getOwnerComponent().getRouter().navTo("RouteMain");
+		},
+		
+		capturePic: function () {
+			var that = this;
+			this.cameraDialog = new Dialog({
+				title: "Click on Capture to take a photo",
+				beginButton: new sap.m.Button({
+					text: "Capture",
+					press: function (oEvent) {
+						that.imageValue = document.getElementById("player");
+						var oButton = oEvent.getSource();
+						that.imageText = oButton.getParent().getContent()[1].getValue();
+						that.cameraDialog.close();
+					}
+				}),
+				content: [
+					new sap.ui.core.HTML({
+						content: "<video id='player' autoplay></video>"
+					}),
+					new sap.m.Input({
+						placeholder: "Please input image text here",
+						required: true
+					})
+				],
+				endButton: new sap.m.Button({
+					text: "Cancel",
+					press: function () {
+						that.cameraDialog.close();
+					}
+				})
+
+			});
+			this.getView().addDependent(this.cameraDialog);
+			this.cameraDialog.open();
+			this.cameraDialog.attachBeforeClose(this.setImage, this);
+			if (navigator.mediaDevices) {
+				navigator.mediaDevices.getUserMedia({
+					video: true,
+					//facingMode: "environment" 
+				}).then(function (stream) {
+					player.srcObject = stream;
+				});
+			}
+		},
+
+		setImage: function () {
+			var oVBox = this.getView().byId("vBox1");
+			var oItems = oVBox.getItems();
+			var imageId = 'archie-' + oItems.length;
+			var fileName = this.imageText;
+			var imageValue = this.imageValue;
+			if (imageValue == null) {
+				MessageToast.show("No image captured");
+			} else {
+
+				var oCanvas = new sap.ui.core.HTML({
+					content: "<canvas id='" + imageId + "' width='320px' height='320px' " +
+						" style='2px solid red'></canvas> "
+				});
+				var snapShotCanvas;
+
+				oVBox.addItem(oCanvas);
+				oCanvas.addEventDelegate({
+					onAfterRendering: function () {
+						snapShotCanvas = document.getElementById(imageId);
+						var oContext = snapShotCanvas.getContext('2d');
+						oContext.drawImage(imageValue, 0, 0, snapShotCanvas.width, snapShotCanvas.height);
+						var imageData = snapShotCanvas.toDataURL('image/png');
+						var imageBase64 = imageData.substring(imageData.indexOf(",") + 1);
+						//	window.open(imageData);  --Use this if you dont want to use third party download.js file 
+						download(imageData, fileName + ".png", "image/png");
+
+					}
+				});
+
+			}
 		}
 
 		/**
